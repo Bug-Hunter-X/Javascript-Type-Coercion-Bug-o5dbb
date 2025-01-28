9bef50ff557554af1dfea2c1967858b1e91abe47
BugHunter-X <askzenai@gmail.com>
2025-01-28 22:14:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,2 @@
+function foo(a,b){return a+b;}console.log(foo(1, "2")); //Output: 12
+This happens because Javascript does not throw an error when you try to add a number and a string. Instead, it converts the number to a string and then concatenates them.
